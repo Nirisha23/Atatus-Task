2671202aa33de03c3d22548e478cabc7a980bb62
Nirisha <nirisha.n@outlook.com>
2024-04-26 12:43:49 +0530

    Last commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 
 function Sidebar() {
+    const location = useLocation();
+
+    const isActive = (pathname) => {
+        // Check if the current location matches the provided pathname
+        return location.pathname === pathname;
+    };
     return (
         <div className='sidebar-container'>
             <h1 className='logo'>The Log</h1>
@@ -9,23 +15,23 @@ function Sidebar() {
                 <div className="sidebar-content-list">
                     <h2>Apache</h2>
                     <div className="sidebar-content-child-list">
-                        <Link to="/">Access Logs</Link >
-                        <Link to="/errorlogs">Error Logs</Link >
-                        <Link to="/attacklogs">Attack Logs</Link >
+                        <NavLink to="/" className={isActive("/") ? "active-link" : ""}>Access Logs</NavLink >
+                        <NavLink to="/errorlogs" className={isActive("/errorlogs") ? "active-link" : ""}>Error Logs</NavLink >
+                        <NavLink to="/attacklogs" className={isActive("/attacklogs") ? "active-link" : ""}>Attack Logs</NavLink >
                     </div>
                 </div>
                 <div className="sidebar-content-list">
                     <h2>MySQL</h2>
                     <div className="sidebar-content-child-list">
-                        <Link to="/startuplogs">Startup Logs</Link >
-                        <Link to="/shutdownlogs">Shutdown Logs</Link >
+                        <NavLink to="/startuplogs" className={isActive("/startuplogs") ? "active-link" : ""}>Startup Logs</NavLink >
+                        <NavLink to="/shutdownlogs" className={isActive("/shutdownlogs") ? "active-link" : ""}>Shutdown Logs</NavLink >
                     </div>
                 </div>
                 <div className="sidebar-content-list">
                     <h2>Nginx</h2>
                     <div className="sidebar-content-child-list">
-                        <Link to="/ngaccesslogs">Access Logs</Link >
-                        <Link to="/ngerrorlogs">Error Logs</Link >
+                        <NavLink to="/ngaccesslogs" className={isActive("/ngaccesslogs") ? "active-link" : ""}>Access Logs</NavLink >
+                        <NavLink to="/ngerrorlogs" className={isActive("/ngerrorlogs") ? "active-link" : ""}>Error Logs</NavLink >
                     </div>
                 </div>
             </div>
